3e13201f9d6916db2f6db02ff687326c4c5995b9
Helcius Araujo Nobrega Cabral <helcius.cabral@hotmail.com>
2024-03-03 14:48:45 -0300

    feat: implementação dos arquivos de login feitos em paralelo

--- cypress/e2e/3_relatorio/relatorio.cy.js
+++ cypress/e2e/3_relatorio/relatorio.cy.js
@@ -8,49 +8,49 @@ describe("Relatório", { viewportHeight: 1080, viewportWidth: 1920 }, () => {
   })
 
   it("Test 01 - Validar dados do Estagiário", () => {
-    cy.fazerLogin(loginData.instrutor.email, loginData.instrutor.senha)
+    cy.fazerLoginComSucesso(loginData.instrutor.email, loginData.instrutor.senha)
     cy.validarDadosPrimeiroDaLista()
   })
 
   it("Test 02 - Validar mudança de abas de skills", () => {
-    cy.fazerLogin(loginData.instrutor.email, loginData.instrutor.senha)
+    cy.fazerLoginComSucesso(loginData.instrutor.email, loginData.instrutor.senha)
     cy.validarDadosPrimeiroDaLista()
     cy.validarTrocarAba()
   })
 
   it("Test 03 - Validar clique na aba atual", () => {
-    cy.fazerLogin(loginData.instrutor.email, loginData.instrutor.senha)
+    cy.fazerLoginComSucesso(loginData.instrutor.email, loginData.instrutor.senha)
     cy.validarDadosPrimeiroDaLista()
     cy.validarPermanecerMesmaAba()
   })
 
   it("Test 04 - Validar indicador de aba atual", () => {
-    cy.fazerLogin(loginData.instrutor.email, loginData.instrutor.senha)
+    cy.fazerLoginComSucesso(loginData.instrutor.email, loginData.instrutor.senha)
     cy.validarDadosPrimeiroDaLista()
 
     cy.validarIndicadorAbaAtiva()
   })
 
   it("Test 05 - Validar dropdown de módulos", () => {
-    cy.fazerLogin(loginData.instrutor.email, loginData.instrutor.senha)
+    cy.fazerLoginComSucesso(loginData.instrutor.email, loginData.instrutor.senha)
     cy.validarDadosPrimeiroDaLista()
 
     cy.validarDropdown()
   })
   it("Test 06 - Validar opção cliente de elaboração de relatório", () => {
-    cy.fazerLogin(loginData.instrutor.email, loginData.instrutor.senha)
+    cy.fazerLoginComSucesso(loginData.instrutor.email, loginData.instrutor.senha)
     cy.validarDadosPrimeiroDaLista()
     cy.selecionarBotaoGerarRelatorio()
   })
   it("Test 07 - Validar opção GP de elaboração de relatório", () => {
-    cy.fazerLogin(loginData.gestao.email, loginData.gestao.senha)
+    cy.fazerLoginComSucesso(loginData.gestao.email, loginData.gestao.senha)
     cy.validarDadosPrimeiroDaLista()
     cy.selecionarBotaoGerarRelatorio()
   })
   it("Test 08 - Validar opção Instrutor de elaboração de relatório", () => {
-    cy.fazerLogin(loginData.instrutor.email, loginData.instrutor.senha)
+    cy.fazerLoginComSucesso(loginData.instrutor.email, loginData.instrutor.senha)
     cy.validarDadosPrimeiroDaLista()
     cy.selecionarBotaoGerarRelatorio()
   })
-  //3 últimos testes aguardando a funcionalidade
+  //3 últimos testes aguardando a funcionalidade do menu do relatorio
 })
--- cypress/support/page/login.page.js
+++ cypress/support/page/login.page.js
@@ -1,9 +1,59 @@
 /* Atributos */
 const { selector } = require("./elements/selector")
-const { email, senha, btnLogout } = selector.login
+const { email, senha, btnLogin, textTelaLogin, textDashboard, textAlert, textEmailInvalido, textSenhaVazia } = selector.login
+import { ListaEmailsInvalidos, ListaSenhasInvalidas, emailInvalido, senhaDinamica } from "../../utils/DataGenerator"
 
 /* Rotinas */
-Cypress.Commands.add("fazerLogin", (email, senha) => {
+
+Cypress.Commands.add("fazerLoginComSucesso", (emailLogin, senhaLogin) => {
+        cy.get(textTelaLogin).contains("Login")
+        cy.get(email).type(emailLogin)
+        cy.get(senha).type(senhaLogin)
+        cy.get(btnLogin).click()
+        cy.get(textDashboard).contains("PDI - DASHBOARD")
+    })
+
+Cypress.Commands.add("fazerLoginComDadosInvalidos", () => {
+    cy.get(email).type(emailInvalido)
+    cy.get(senha).type(senhaDinamica)
+    cy.get(btnLogin).click()
+    cy.get(textAlert).contains("Verifique se seus dados estão corretos")
 })
 
-Cypress.Commands.add("fazerLogout", () => {})
+Cypress.Commands.add("fazerLoginComEmailVazio", () => {
+    cy.get(email).clear()
+    cy.get(senha).type(senhaDinamica)
+    cy.get(btnLogin).click()
+    cy.get(textEmailInvalido).contains("O email é obrigatório")
+})
+
+Cypress.Commands.add("fazerLoginComEmailInvalido", () => {
+    cy.get(email).clear()
+    cy.get(email).type(ListaEmailsInvalidos[Math.floor(Math.random() * ListaEmailsInvalidos.length)])
+    cy.get(senha).type(senhaDinamica)
+    cy.get(btnLogin).click()
+    cy.get(textEmailInvalido).contains("Formato de email inválido")
+})
+
+Cypress.Commands.add("fazerLoginSenhaInvalida", (emailLogin) => {
+        cy.get(textTelaLogin).contains("Login")
+        cy.get(email).type(emailLogin)
+        cy.get(senha).type(senhaDinamica)
+        cy.get(btnLogin).click()
+        cy.get(textAlert).contains("Verifique se seus dados estão corretos")
+    })
+
+Cypress.Commands.add("fazerLoginComSenhaEmBranco", () => {
+    cy.get(email).type(emailInvalido)
+    cy.get(senha).clear()
+    cy.get(btnLogin).click()
+    cy.get(textSenhaVazia).contains("A senha é obrigatória")
+})
+
+Cypress.Commands.add("fazerLoginComSenhaEmFormatoInvalido", (emailLogin) => {
+        cy.get(textTelaLogin).contains("Login")
+        cy.get(email).type(emailLogin)
+        cy.get(senha).type(ListaSenhasInvalidas[Math.floor(Math.random() * ListaSenhasInvalidas.length)])
+        cy.get(btnLogin).click()
+        cy.get(textAlert).contains("Verifique se seus dados estão corretos")
+    })
