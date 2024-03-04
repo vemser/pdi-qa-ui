4824c8d0efb2576e8db13463ac741538facd2b13
Pedro Bersch <pedroleottebersch@gmail.com>
2024-03-04 15:50:06 -0300

    fix: testes de autorização agora preservao dados sensiveis em anonimo

--- cypress/e2e/1_autenticacao/logout.cy.js
+++ cypress/e2e/1_autenticacao/logout.cy.js
@@ -1,17 +1,15 @@
 /// <reference types="cypress" />
 
-import loginData from "../../fixtures/login.json"
-
 describe("Logout", () => {
   beforeEach(() => {
     cy.visit("/")
   })
 
   it("Test 01 - Verifique se é possível fazer logout corretamente no responsivo", () => {
-    cy.fazerLogoutResponsivo(loginData.instrutor.email, loginData.instrutor.senha)
+    cy.fazerLogoutResponsivo(Cypress.env(`INSTRUTOR_EMAIL`), Cypress.env(`INSTRUTOR_SENHA`))
   })
 
   it("Test 02 - Verifique se é possível fazer logout corretamente",{viewportHeight: 1080, viewportWidth: 1920},() => {
-    cy.fazerLogout(loginData.instrutor.email, loginData.instrutor.senha)
+    cy.fazerLogout(Cypress.env(`INSTRUTOR_EMAIL`), Cypress.env(`INSTRUTOR_SENHA`))
   })
 })
