34c91b73ebf3da189c99a942e8096d7392a09100
Helcius Araújo Nóbrega Cabral <helcius.cabral@dbccompany.com.br>
2024-03-05 17:12:38 -0300

    Feat: reformulação dos testes de agendamento, de acordo com escopo de features

--- cypress/support/page/agendamento.page.js
+++ cypress/support/page/agendamento.page.js
@@ -13,12 +13,14 @@ const {
   seletorDataInicio,
   seletorDataFim,
   seletorFiltroEdicao,
+  btnFiltrar,
   seletorFiltroEdicaoPrimeiraOpcao,
   txtTituloPrimeiroElemento,
   txtDataInicioPrimeiroElemento,
   txtDataFimPrimeiroElemento,
   txtStatusPrimeiroElemento,
   btnDetalhes,
+  modalDetalhes,
   txtDetalhesTitulo,
   txtDetalhesData,
   txtDetalhesStatus,
@@ -30,8 +32,9 @@ const {
 Cypress.Commands.add("validaDataInicioDataFim", (dataInicio, dataFim) => {
   cy.get(seletorDataInicio).type(dataInicio)
   cy.get(seletorDataFim).type(dataFim)
-  //cy.get(seletorFiltroEdicao).click()
-  //cy.get(seletorFiltroEdicaoPrimeiraOpcao).click()
+  cy.get(seletorFiltroEdicao).click()
+  cy.get(seletorFiltroEdicaoPrimeiraOpcao).first().click()
+  cy.get(btnFiltrar).click()
   cy.get(txtDataInicioPrimeiroElemento, { timeout: 120000 }).contains(dataInicio)
   cy.get(txtDataFimPrimeiroElemento).contains(dataFim)
 })
@@ -42,4 +45,9 @@ Cypress.Commands.add("acessarPaginaAcompanhamento", () => {
 
 Cypress.Commands.add("validaMensagemRegistrosVazios", () => {
   cy.get(txtMensagemRegistroVazio).contains("Nenhum registro encontrado")
+})
+
+Cypress.Commands.add("validarDetalhesAgendamento", () => {
+  cy.get(btnDetalhes).first().click()
+  cy.get(modalDetalhes).should("exist")
 })
--- cypress/support/page/elements/agendamento.js
+++ cypress/support/page/elements/agendamento.js
@@ -3,6 +3,7 @@ export const agendamento = {
   seletorDataFim: 'input[name="dateEnd"]',
   seletorFiltroEdicao: "#demo-simple-select",
   seletorFiltroEdicaoPrimeiraOpcao: 'li[data-qa="select-option-vemser-12"]',
+  btnFiltrar: 'data-qa="filter-button"',
   txtTituloPrimeiroElemento:
     "#root > main > section > section > main > div > div > div.MuiDataGrid-root.MuiDataGrid-autoHeight.MuiDataGrid-root--densityStandard.MuiDataGrid-withBorderColor.css-1lo9hqi > div > div.MuiDataGrid-virtualScroller.css-1pzb349 > div > div > div:nth-child(1) > div:nth-child(2) > div",
   txtDataInicioPrimeiroElemento:
@@ -11,8 +12,8 @@ export const agendamento = {
     "#root > main > section > section > main > div > div > div.MuiDataGrid-root.MuiDataGrid-autoHeight.MuiDataGrid-root--densityStandard.MuiDataGrid-withBorderColor.css-1lo9hqi > div > div.MuiDataGrid-virtualScroller.css-1pzb349 > div > div > div:nth-child(1) > div:nth-child(4) > div",
   txtStatusPrimeiroElemento:
     "#root > main > section > section > main > div > div > div.MuiDataGrid-root.MuiDataGrid-autoHeight.MuiDataGrid-root--densityStandard.MuiDataGrid-withBorderColor.css-10djrmb > div > div.MuiDataGrid-virtualScroller.css-1pzb349 > div > div > div:nth-child(1) > div:nth-child(5) > div > div > span",
-  btnDetalhes:
-    "#root > main > section > section > main > div > div > div.MuiDataGrid-root.MuiDataGrid-autoHeight.MuiDataGrid-root--densityStandard.MuiDataGrid-withBorderColor.css-1lo9hqi > div > div.MuiDataGrid-virtualScroller.css-1pzb349 > div > div > div:nth-child(1) > div:nth-child(6) > button",
+  btnDetalhes: 'button[data-qa="button-details"]',
+  modalDetalhes: 'div[data-qa="modal-details"]',
   txtDetalhesTitulo: "#:r92:",
   txtDetalhesData:
     "body > div.MuiDialog-root.MuiModal-root.css-126xj0f > div.MuiDialog-container.MuiDialog-scrollPaper.css-ekeie0 > div > div > p:nth-child(2)",
