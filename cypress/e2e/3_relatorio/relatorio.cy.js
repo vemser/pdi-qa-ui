d96073535aaf98e0719b653b649f6aba1652fb06
Helcius Araújo Nóbrega Cabral <helcius.cabral@dbccompany.com.br>
2024-03-05 15:14:53 -0300

    Feat: Finalização e correção dos testes de relatório, de acordo com o escopo final do projeto

--- cypress/e2e/3_relatorio/relatorio.cy.js
+++ cypress/e2e/3_relatorio/relatorio.cy.js
@@ -1,17 +1,14 @@
 /// <reference types="cypress" />
 
-
 describe("Relatório", { viewportHeight: 1080, viewportWidth: 1920 }, () => {
   beforeEach(() => {
     cy.visit("/")
   })
 
-  
   it("Test 01 - Validar dados do Estagiário", () => {
     cy.fazerLoginComSucesso(Cypress.env(`INSTRUTOR_EMAIL`), Cypress.env(`INSTRUTOR_SENHA`))
     cy.validarDadosPrimeiroDaLista()
   })
-  
 
   it("Test 02 - Validar mudança de abas de skills", () => {
     cy.fazerLoginComSucesso(Cypress.env(`INSTRUTOR_EMAIL`), Cypress.env(`INSTRUTOR_SENHA`))
@@ -28,30 +25,17 @@ describe("Relatório", { viewportHeight: 1080, viewportWidth: 1920 }, () => {
   it("Test 04 - Validar indicador de aba atual", () => {
     cy.fazerLoginComSucesso(Cypress.env(`INSTRUTOR_EMAIL`), Cypress.env(`INSTRUTOR_SENHA`))
     cy.navegarDetalhesPrimeiroDaLista()
-
     cy.validarIndicadorAbaAtiva()
   })
 
   it("Test 05 - Validar dropdown de módulos", () => {
     cy.fazerLoginComSucesso(Cypress.env(`INSTRUTOR_EMAIL`), Cypress.env(`INSTRUTOR_SENHA`))
     cy.navegarDetalhesPrimeiroDaLista()
-
     cy.validarDropdown()
   })
-  it("Test 06 - Validar opção cliente de elaboração de relatório", () => {
+  it("Test 06 - Validar opção de elaboração de relatório", () => {
     cy.fazerLoginComSucesso(Cypress.env(`INSTRUTOR_EMAIL`), Cypress.env(`INSTRUTOR_SENHA`))
     cy.navegarDetalhesPrimeiroDaLista()
     cy.selecionarBotaoGerarRelatorio()
   })
-  it("Test 07 - Validar opção GP de elaboração de relatório", () => {
-    cy.fazerLoginComSucesso(Cypress.env(`INSTRUTOR_EMAIL`), Cypress.env(`INSTRUTOR_SENHA`))
-    cy.navegarDetalhesPrimeiroDaLista()
-    cy.selecionarBotaoGerarRelatorio()
-  })
-  it("Test 08 - Validar opção Instrutor de elaboração de relatório", () => {
-    cy.fazerLoginComSucesso(Cypress.env(`INSTRUTOR_EMAIL`), Cypress.env(`INSTRUTOR_SENHA`))
-    cy.navegarDetalhesPrimeiroDaLista()
-    cy.selecionarBotaoGerarRelatorio()
-  })
-  //3 últimos testes aguardando a funcionalidade do menu do relatorio
 })
